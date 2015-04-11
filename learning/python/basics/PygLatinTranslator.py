5614e33ae73246b916c45930bb0fad09399aac25
Arthy Vijayaraghavan <art84parthu@gmail.com>
2015-04-11 22:27:07 +0530

    added additional isalpha check

--- learning/python/basics/PygLatinTranslator.py
+++ learning/python/basics/PygLatinTranslator.py
@@ -1,8 +1,8 @@
 def pyg_latin():
     user_input = raw_input("Enter a word in English");
     user_input = user_input.strip();
-    if user_input == '':
-        print("Enter valid word. Try again.. ");
+    if ((user_input == '') or (not user_input.isalpha())) :
+        print("Enter a valid word with just alphabets. Lets try again.. ");
         pyg_latin();
     else:
         trans = user_input[1:] + user_input[0] + "ay";
